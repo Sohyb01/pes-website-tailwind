bf93c556200318692c37f3b9d63e9e43b7356fe3
Sohyb01 <sohyb0155@gmail.com>
2023-07-17 16:15:10 +0300

    Getting Started...

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1 className="text-base text-pes_darkbluegray">Test</h1>
+            <p className="text-2xl font-bold text-pes_white bg-pes_darkblue">
+                Programming is now much easier
+            </p>
+        </>
+    )
+}
+
+export default App
