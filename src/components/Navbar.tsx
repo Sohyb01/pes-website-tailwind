7ef9f01ce7f9620a2e4a1e7e7e448a2fe807c944
Sohyb01 <sohyb0155@gmail.com>
2023-07-21 17:03:55 +0300

    Working on navigation and navbar dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                             >
                                 Contact
                             </a>
-                            <a
+                            {/* <a
                                 href=""
                                 className="text-md  flex items-center gap-2 fill-pes_white text-pes_white duration-200 hover:fill-pes_orange hover:text-pes_orange"
                             >
@@ -37,7 +37,65 @@ const Navbar = () => {
                                         <path d="M5.9996 8C5.78029 8 5.56087 7.90234 5.39373 7.70703L0.251105 1.70703C-0.0837016 1.31641 -0.0837016 0.683594 0.251105 0.292969C0.585911 -0.0976562 1.1283 -0.0976562 1.4631 0.292969L5.9996 5.5875L10.5369 0.29375C10.8717 -0.0968752 11.4141 -0.0968752 11.7489 0.29375C12.0837 0.684375 12.0837 1.31719 11.7489 1.70781L6.60627 7.70781C6.43886 7.90312 6.21923 8 5.9996 8Z" />
                                     </svg>
                                 </div>
-                            </a>
+                            </a> */}
+                            <div className="dropdown dropdown-end">
+                                <label
+                                    tabIndex={0}
+                                    className="m-1 text-pes_white duration-200 hover:text-pes_orange"
+                                >
+                                    More
+                                </label>
+                                <ul
+                                    tabIndex={0}
+                                    className="menu dropdown-content rounded-box z-[50] h-fit bg-base-100"
+                                >
+                                    <li className="text-pes_black duration-200 hover:bg-pes_orange hover:text-pes_white">
+                                        <a className="hover:text-pes_white">
+                                            Become an Instructor
+                                        </a>
+                                    </li>
+                                    <li className="text-pes_black duration-200 hover:bg-pes_orange hover:text-pes_white">
+                                        <a className="hover:text-pes_white">
+                                            Become an Instructor
+                                        </a>
+                                    </li>
+                                    <li className="text-pes_black duration-200 hover:bg-pes_orange hover:text-pes_white">
+                                        <a className="hover:text-pes_white">
+                                            Online courses
+                                        </a>
+                                    </li>
+                                    <li className="text-pes_black duration-200 hover:bg-pes_orange hover:text-pes_white">
+                                        <a className="hover:text-pes_white">
+                                            Partnerships
+                                        </a>
+                                    </li>
+                                    <li className="text-pes_black duration-200 hover:bg-pes_orange hover:text-pes_white">
+                                        <a className="hover:text-pes_white">
+                                            Join as student
+                                        </a>
+                                    </li>
+                                    <li className="text-pes_black duration-200 hover:bg-pes_orange hover:text-pes_white">
+                                        <a className="hover:text-pes_white">
+                                            Certifications
+                                        </a>
+                                    </li>
+                                    <li className="text-pes_black duration-200 hover:bg-pes_orange hover:text-pes_white">
+                                        <a className="hover:text-pes_white">
+                                            Franchises
+                                        </a>
+                                    </li>
+                                    <li className="text-pes_black duration-200 hover:bg-pes_orange hover:text-pes_white">
+                                        <a className="hover:text-pes_white">
+                                            Leaderboard
+                                        </a>
+                                    </li>
+                                    <li className="text-pes_black duration-200 hover:bg-pes_orange hover:text-pes_white">
+                                        <a className="text-inherit">
+                                            Free trail
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                         <button className="text-md h-fit rounded-full bg-pes_orange px-8 py-2 text-pes_white duration-200 hover:bg-orange-300 md:py-4">
                             Login
